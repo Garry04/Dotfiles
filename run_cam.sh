d5f52ff0eb9ba403a1bec0a406508409b43ae056
garry04 <garryedits@gmail.com>
2024-11-06 23:58:56 +0100

    update

--- run_cam.sh
+++ run_cam.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 source ~/Desktop/secCam/.venv/bin/activate
 dunstify --icon=camera --appname=Camera --urgency=normal "Camera" "Camera starting..."
-python ~/Desktop/secCam/cam.py
+python ~/Desktop/secCam/cam v2.py
 dunstify --icon=camera --appname=Camera --urgency=normal "Camera" "Camera stopped"
